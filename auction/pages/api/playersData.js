e74be4dcf8d12566f6018c17e47bcaef6885c6fa
Agrim <agrimsharma1714@gmail.com>
2023-05-10 23:54:35 +0530

    initial

---
+++ auction/pages/api/playersData.js
@@ -0,0 +1,327 @@
+export default function handler(req, res) {
+  res.status(200).json([
+
+        {
+          "id": 1,
+          "player": "Virat Kohli",
+          "category": "Batsman"
+        },
+        {
+          "id":2,
+          "player": "Rohit Sharma",
+          "category": "Batsman"
+        },
+        {
+          "id":3,
+          "player": "Shikhar Dhawan",
+          "category": "Batsman"
+        },
+        {
+          "id":4,
+          "player": "KL Rahul",
+          "category": "Batsman"
+        },
+        {
+          "id":5,
+          "player": "Suryakumar Yadav",
+          "category": "Batsman"
+        },
+        {
+          "id":6,
+          "player": "Shubman Gill",
+          "category": "Batsman"
+        },
+        {
+          "id":7,
+          "player": "Ajinkya Rahane",
+          "category": "Batsman"
+        },
+        {
+          "id":8,
+          "player": "Hanuma Vihari",
+          "category": "Batsman"
+        },
+        {
+          "id":9,
+          "player": "Mayank Agarwal",
+          "category": "Batsman"
+        },
+        {
+          "id":10,
+          "player": "Prithvi Shaw",
+          "category": "Batsman"
+        },
+        {
+          "id":11,
+          "player": "Manish Pandey",
+          "category": "Batsman"
+        },
+        {
+          "id":12,
+          "player": "Sanju Samson",
+          "category": "Batsman"
+        },
+        {
+          "id":13,
+          "player": "Ishan Kishan",
+          "category": "Batsman"
+        },
+        {
+          "id":14,
+          "player": "Rishabh Pant",
+          "category": "Batsman"
+        },
+        {
+          "id":15,
+          "player": "Kedar Jadhav",
+          "category": "Batsman"
+        },
+        {
+          "id":16,
+          "player": "Ambati Rayudu",
+          "category": "Batsman"
+        },
+        {
+          "id":17,
+          "player": "Yuvraj Singh",
+          "category": "Batsman"
+        },
+        {
+          "id":18,
+          "player": "Suresh Raina",
+          "category": "Batsman"
+        },
+        {
+          "id":19,
+          "player": "Rahul Dravid",
+          "category": "Batsman"
+        },
+        {
+          "id":20,
+          "player": "Sachin Tendulkar",
+          "category": "Batsman"
+        },
+        {
+          "id":21,
+          "player": "Sourav Ganguly",
+          "category": "Batsman"
+        },
+
+    
+
+
+
+
+
+        {
+          "id":22,
+          "player": "Jasprit Bumrah",
+          "category": "Bowler",
+        },
+        {
+          "id":23,
+          "player": "Mohammad Shami",
+          "category": "Bowler",
+        },
+        {
+          "id":24,
+          "player": "Bhuvneshwar Kumar",
+          "category": "Bowler",
+        },
+        {
+          "id":25,
+          "player": "Umesh Yadav",
+          "category": "Bowler",
+        },
+        {
+          "id":26,
+          "player": "Ishant Sharma",
+          "category": "Bowler",
+        },
+        {
+          "id":27,
+          "player": "Yuzvendra Chahal",
+          "category": "Bowler",
+        },
+        {
+          "id":28,
+          "player": "Kuldeep Yadav",
+          "category": "Bowler",
+        },
+        {
+          "id":29,
+          "player": "Arshdeep Singh",
+          "category": "Bowler",
+        },
+        {
+          "id":30,
+          "player": "Deepak Chahar",
+          "category": "Bowler",
+        },
+        {
+          "id":31,
+          "player": "Shardul Thakur",
+          "category": "Bowler",
+        },
+        {
+          "id":32,
+          "player": "Navdeep Saini",
+          "category": "Bowler",
+        },
+        {
+          "id":33,
+          "player": "T. Natarajan",
+          "category": "Bowler",
+        },
+        {
+          "id":34,
+          "player": "Jasprit Bumrah",
+          "category": "Bowler",
+        },
+        {
+          "id":35,
+          "player": "Mohammed Siraj",
+          "category": "Bowler",
+        },
+        {
+          "id":36,
+          "player": "Varun Chakravarthy",
+          "category": "Bowler",
+        },
+        {
+          "id":37,
+          "player": "Shivam Mavi",
+          "category": "Bowler",
+        },
+        {
+          "id":38,
+          "player": "Kamlesh Nagarkoti",
+          "category": "Bowler",
+        },
+        {
+          "id":39,
+          "player": "Khaleel Ahmed",
+          "category": "Bowler",
+        },
+        {
+          "id":40,
+          "player": "Jaydev Unadkat",
+          "category": "Bowler",
+        },
+
+    
+
+
+
+
+
+
+
+        {
+          "id":41,
+          "player": "Ben Stokes",
+          "category": "All-rounders",
+        },
+        {
+          "id":42,
+          "player": "Shakib Al Hasan",
+          "category": "All-rounders",
+        },
+        {
+          "id":43,
+          "player": "Hardik Pandya",
+          "category": "All-rounders",
+        },
+        {
+          "id":44,
+          "player": "Ravichandran Ashwin",
+          "category": "All-rounders",
+        },
+        {
+          "id":45,
+          "player": "Andre Russell",
+          "category": "All-rounders",
+        },
+        {
+          "id":46,
+          "player": "Jason Holder",
+          "category": "All-rounders",
+        },
+        {
+          "id":47,
+          "player": "Chris Woakes",
+          "category": "All-rounders",
+        },
+        {
+          "id":48,
+          "player": "Dwayne Bravo",
+          "category": "All-rounders",
+        },
+        {
+          "id":49,
+          "player": "Glenn Maxwell",
+          "category": "All-rounders",
+        },
+        {
+          "id":50,
+          "player": "Moeen Ali",
+          "category": "All-rounders",
+        },
+        {
+          "id":51,
+          "player": "Mitchell Marsh",
+          "category": "All-rounders",
+        },
+        {
+          "id":52,
+          "player": "Kieron Pollard",
+          "category": "All-rounders",
+        },
+        {
+          "id":53,
+          "player": "Thisara Perera",
+          "category": "All-rounders",
+        },
+        {
+          "id":54,
+          "player": "Angelo Mathews",
+          "category": "All-rounders",
+        },
+        {
+          "id":55,
+          "player": "Mohammad Nabi",
+          "category": "All-rounders",
+        },
+        {
+          "id":56,
+          "player": "Ravindra Jadeja",
+          "category": "All-rounders",
+        },
+        {
+          "id":57,
+          "player": "Washington Sundar",
+          "category": "All-rounders",
+        },
+        {
+          "id":58,
+          "player": "Axar Patel",
+          "category": "All-rounders",
+        },
+        {
+          "id":59,
+          "player": "Krunal Pandya",
+          "category": "All-rounders",
+        },
+        {
+          "id":60,
+          "player": "Vijay Shankar",
+          "category": "All-rounders",
+        },
+        {
+          "id":61,
+          "player": "Rishi Dhawan",
+          "category": "All-rounders",
+        },
+  
+  ]);
+}
---
+++ auction/pages/index.js
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Players from "@/pages/components/Players";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home({ playersData }) {
+  return (
+    <div className="bg-gradient-to-r from-[#070A60] to-[#103ED1]  h-[100vh] w-[100vw] p-5 ">
+      <div className="">
+        <div className="font-extrabold text-4xl flex items-center justify-center text-slate-400">
+          AUCTION
+        </div>
+        <div className="grid grid-cols-2 p-5 items-center m-auto lg:flex lg:justify-evenly lg:mt-10 lg:space-x-5">
+          <div
+            className="border rounded-lg bg-green-600 p-2 flex items-center justify-center lg:w-full text-lg font-bold  m-1 hover:bg-green-500 text-black transition duration-300 hover:scale-105"
+            onClick={() => filterItem("batsmen")}
+          >
+            Batsmen
+          </div>
+          <div className="border rounded-lg bg-green-600 p-2 flex items-center justify-center lg:w-full text-lg font-bold  m-1 hover:bg-green-500 text-black transition duration-300 hover:scale-105">
+            Bowlers
+          </div>
+          <div className="border rounded-lg bg-green-600 p-2 flex items-center justify-center lg:w-full text-lg font-bold  m-1 hover:bg-green-500 text-black transition duration-300 hover:scale-105">
+            Weeketkeepers
+          </div>
+          <div className="border rounded-lg bg-green-600 p-2 flex items-center justify-center lg:w-full text-lg font-bold  m-1 hover:bg-green-500 text-black transition duration-300 hover:scale-105">
+            All-rounders
+          </div>
+        </div>
+      </div>
+
+      <div>
+        {playersData?.map(({id, player, category}) => {
+          return (
+            <Players key={id} player={player} category={category} />
+          )
+        })}
+      </div>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:3000/api/playersData");
+  const playersData = await res.json();
+
+  return {
+    props: {
+      playersData,
+    },
+  };
+}
